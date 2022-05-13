3b5acffc82a743636d19b10a86fde88027144afe
Thibaut Van Spaandonck <thibaut@urge2code.com>
2022-05-13 17:46:30 +0200

    Refactored rollup config + npm scripts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ui-utils
+
 UI utilities library
+
+# Scripts
+
+- _Build:_ `yarn run build`
+- _Publish package:_ `yarn run deploy`
+- _Run tests:_ `yarn run test`
--- rollup.config.js
+++ rollup.config.js
@@ -1,73 +1,22 @@
-import cleaner from 'rollup-plugin-cleaner'
-
-export default [
-  {
-    input: 'src/jest/index.js',
-    output: {
-      file: 'dist/jest/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins: [
-      cleaner({
-        targets: ['dist'],
-      }),
-    ],
-  },
-  {
-    input: 'src/responsive/index.js',
-    output: {
-      file: 'dist/responsive/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/color.js',
-    output: {
-      file: 'dist/color/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/date.js',
-    output: {
-      file: 'dist/date/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/formatting.js',
-    output: {
-      file: 'dist/formatting/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/use-file-export.js',
-    output: {
-      file: 'dist/file/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/use-query-string.js',
-    output: {
-      file: 'dist/url/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
-  {
-    input: 'src/validation.js',
-    output: {
-      file: 'dist/validation/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  },
+const entryPoints = [
+  'src/jest/index.js',
+  'src/responsive/index.js',
+  'src/color.js',
+  'src/date.js',
+  'src/formatting.js',
+  'src/file.js',
+  'src/url.js',
+  'src/validation.js',
 ]
+
+export default entryPoints.map((entryPoint) => ({
+  input: entryPoint,
+  output: {
+    file: `dist/${entryPoint
+      .replace('src/', '')
+      .replace('/index.js', '')
+      .replace('.js', '')}/index.js`,
+    format: 'cjs',
+    sourcemap: true,
+  },
+}))
